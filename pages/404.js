c2fc878806a51db309b7b71b7bc2330ec6fc6e2e
Ian Jones <ian@egghead.io>
2019-05-08 23:13:58 -0400

    move folders around

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
-import Layout from '../components/Layout'
-import SEO from '../components/seo'
+import Layout from '../src/components/Layout'
+import SEO from '../src/components/seo'
 
 class NotFoundPage extends React.Component {
   render() {
