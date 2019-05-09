cbf3e5b02b22f1fbd1aa500a07f006bfb34e7afe
Ian Jones <ian@egghead.io>
2019-05-09 00:38:20 -0400

    get digital garden rendering

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,75 +3,22 @@ module.exports = {
     title: `Ian Jones' Blog`,
     author: `Ian Jones`,
     description: `I write on things that interest me, mostly web development.`,
-    siteUrl: `https://ianjones.rocks/`,
+    siteUrl: `https://ianjones.us/`,
     social: {
       twitter: `_jonesian`,
     },
   },
-  plugins: [
+  __experimentalThemes: [
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-theme-digital-garden',
       options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
+        notesPath: '/notes',
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-theme-digital-garden-blog',
       options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Ian Jones Blog`,
-        short_name: `Ian Jones`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#72A2F1`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
+        postsPath: '/writing',
       },
     },
   ],
