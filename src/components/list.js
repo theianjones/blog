00b5426cbfc4922f114ee6c65017240e32c0cce4
ijones16 <jones58ian@gmail.com>
2020-04-22 11:19:05 -0400

    use variant in both places

--- src/components/list.js
+++ src/components/list.js
@@ -1,22 +1,17 @@
 /** @jsx jsx */
-import React from "react"
-import { jsx, Styled, Flex, Image } from "theme-ui"
-import { map } from "lodash"
+import React from 'react'
+import { jsx, Styled, Flex, Image } from 'theme-ui'
+import { map } from 'lodash'
 
 export default function List({ collections = [] }) {
   return (
     <>
-      {map(collections, collection => (
+      {map(collections, (collection) => (
         <Flex
           key={collection.id || collection.slug}
           sx={{
-            alignItems: "center",
-            padding: ".5rem",
-            margin: "0 -0.5rem",
-            borderRadius: 5,
-            ":hover": {
-              backgroundColor: "muted",
-            },
+            alignItems: 'center',
+            variant: 'styles.postlistitem',
           }}
         >
           {collection.image_thumb_url && (
@@ -26,13 +21,13 @@ export default function List({ collections = [] }) {
             key={collection.slug || collection.id}
             sx={{
               marginLeft: 10,
-              color: "text",
-              textDecoration: "none",
+              color: 'text',
+              textDecoration: 'none',
             }}
             href={collection.http_url}
             aria-label={`View ${collection.title}`}
           >
-            {collection.title}{" "}
+            {collection.title}{' '}
           </Styled.a>
         </Flex>
       ))}
