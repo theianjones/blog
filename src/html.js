cca2674f8df3e1e68cd00723fb3538854e4cbc26
ijones16 <jones58ian@gmail.com>
2020-06-05 16:07:45 -0400

    black lives matter

---
+++ src/html.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class HTML extends React.Component {
+  render() {
+    return (
+      <html {...this.props.htmlAttributes}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          {this.props.headComponents}
+        </head>
+        <body {...this.props.bodyAttributes}>
+          <script
+            src="https://unpkg.com/i-stand/black-lives.js"
+            type="module"
+          ></script>
+          <black-lives></black-lives>
+          {this.props.preBodyComponents}
+          <noscript>This site runs best with JavaScript enabled.</noscript>
+          <div
+            key={`body`}
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    )
+  }
+}
